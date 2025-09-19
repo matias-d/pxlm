a62fdd3298552129881e94ea4a9ef4091fce383f
matias-d <matiasc.02.2003@gmail.com>
2025-09-18 22:09:06 -0300

    feat: :rocket: add ganache network and deploy

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -36,6 +36,16 @@ const config: HardhatUserConfig = {
       url: configVariable("SEPOLIA_RPC_URL"),
       accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
     },
+
+    ganache: {
+      type: "http",
+      chainType: "l1",
+      url: "http://127.0.0.1:7545",
+      accounts: [
+        "0xe2d95afbe1d92c567eb99a84aaea67e9f0dbfb1e828ef9ecba997ba1088de8a1",
+        "0x7892162c97437e3ec771a43732d688c7e67fc5dbf05b0de4d48509979fac1b3f",
+      ],
+    },
   },
 };
 
