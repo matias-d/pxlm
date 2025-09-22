13267b4472e90a0215e8aafd6c666c78f4bfa716
matias-d <matiasc.02.2003@gmail.com>
2025-09-21 23:24:11 -0300

    refactor: :memo: component compund

--- client/src/components/ui/pxl-image.tsx
+++ client/src/components/ui/pxl-image.tsx
@@ -1,18 +1,30 @@
 import { cn } from "../../lib/cn";
 
 interface Props {
+  classNameContainer?: string;
   className?: string;
   src: string;
   alt: string;
 }
 
-export default function PXLImage({ alt, src, className }: Props) {
+export default function PXLImage({
+  alt,
+  src,
+  className,
+  classNameContainer,
+}: Props) {
   return (
-    <div className="group relative inline-flex overflow-hidden">
+    <div
+      className={cn(
+        "group relative inline-flex overflow-hidden rounded-md ",
+        classNameContainer
+      )}
+    >
       <img
         src={src}
         alt={alt}
-        className={cn(" object-cover rounded-md shadow", className)}
+        loading="lazy"
+        className={cn(" object-cover shadow w-full", className)}
       />
       <div className="absolute inset-0 flex h-full w-full justify-center [transform:skew(-12deg)_translateX(-100%)] group-hover:duration-1000 group-hover:[transform:skew(-12deg)_translateX(100%)]">
         <div className="relative h-full w-8 bg-white/20"></div>
--- client/src/components/widgets/pxl-card.tsx
+++ client/src/components/widgets/pxl-card.tsx
@@ -1,56 +1,115 @@
-import AvatarUI from "../ui/avatar-ui";
-import PXLImage from "../ui/pxl-image";
-import Button from "../ui/button";
-import { cn } from "@/lib/cn";
-import Card from "../ui/card";
+import { getRarityTier } from "@/helpers/functions/pxl-get-rarity";
 import { Sparkle } from "lucide-react";
+import ButtonUI from "../ui/button";
+import CardUI from "../ui/card";
+import { cn } from "@/lib/cn";
 
-export default function PxlCard() {
+export function Card({ children }: { children: React.ReactNode }) {
   return (
-    <Card className="group relative overflow-hidden hover:scale-[1.01] transition-all duration-300 ease-in-out ">
-      <PXLImage src="/pxl-examples/6.svg" alt="PXL ART" className="w-full " />
-      <div className="flex items-center justify-between my-3">
-        <div className="flex items-center gap-x-1 font-semibold ">
-          <h2 className="text-text-primary  ">PXL ART</h2>
-          <p className="font-display">#0212</p>
-        </div>
-
-        <div className="flex items-center gap-x-1">
-          <p className="font-display text-indigo-500 text-sm font-semibold bg-card-super-light px-1 rounded-md flex items-center gap-x-1">
-            <Sparkle size={15} />
-            #0212
-          </p>
-        </div>
-      </div>
-
-      <div>
-        <div className="flex items-center gap-x-2 font-semibold mb-0.5">
-          <p className="font-display">1.50</p>
-          <p className="text-text-secondary">TBNB</p>
-        </div>
-        <div className="flex items-center gap-x-1 text-xs font-semibold">
-          <p className=" text-text-secondary">Last sale</p>
-          <p className="font-display ">
-            1.40 <span className="text-text-secondary font-primary">TBNB</span>
-          </p>
-        </div>
-      </div>
-
-      <div
-        className={cn(
-          "absolute -bottom-full group-hover:bottom-0 transition-all duration-300 ease-in-out w-full left-0"
-        )}
-      >
-        <Button
-          className="w-full h-14 text-base flex items-center rounded-none"
-          classNameContainer="text-sm flex items-center justify-between w-full font-semibold "
-        >
-          <span>Buy Now</span>
-          <p>
-            1.40 <span className="font-primary">TBNB</span>
-          </p>
-        </Button>
-      </div>
-    </Card>
+    <CardUI className="group relative overflow-hidden hover:scale-[1.01] transition-all duration-300 ease-in-out">
+      {children}
+    </CardUI>
   );
 }
+
+export function FooterContent({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <footer
+      className={cn(
+        "absolute -bottom-full group-hover:bottom-0 transition-all duration-300 ease-in-out w-full left-0",
+        className
+      )}
+    >
+      {children}
+    </footer>
+  );
+}
+
+export function Button({
+  className,
+  classNameContainer,
+  price,
+}: {
+  className?: string;
+  classNameContainer?: string;
+  price: string;
+}) {
+  return (
+    <ButtonUI
+      className={cn(
+        "w-full h-14 text-base flex items-center rounded-none",
+        className
+      )}
+      classNameContainer={cn(
+        "text-sm flex items-center justify-between w-full font-semibold",
+        classNameContainer
+      )}
+    >
+      <span>Buy Now</span>
+      <p>
+        {price} <span className="font-primary">TBNB</span>
+      </p>
+    </ButtonUI>
+  );
+}
+
+export function PriceDetails({ price }: { price: string }) {
+  return (
+    <div>
+      <div className="flex items-center gap-x-2 font-semibold mb-0.5">
+        <p className="font-display">{price}</p>
+        <p className="text-text-secondary">TBNB</p>
+      </div>
+      <div className="flex items-center gap-x-1 text-xs font-semibold">
+        <p className=" text-text-secondary">Last sale</p>
+        <p className="font-display ">
+          1.40 <span className="text-text-secondary font-primary">TBNB</span>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export function Info({
+  rarity_score,
+  tokenId,
+}: {
+  rarity_score: number;
+  tokenId: number;
+}) {
+  const rarityConfig = getRarityTier(rarity_score);
+
+  return (
+    <section className="flex items-center justify-between my-3">
+      <div className="flex items-center gap-x-1 font-semibold ">
+        <h2 className="text-text-primary  ">PXL ART</h2>
+        <p className="font-display">#{tokenId}</p>
+      </div>
+
+      <div className="flex items-center gap-x-1">
+        <p
+          className={cn(
+            "font-display text-sm font-semibold bg-card-super-light px-1 rounded-md flex items-center gap-x-1",
+            rarityConfig.tailwind.text
+          )}
+        >
+          <Sparkle size={15} />#{rarity_score}
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default {
+  Card,
+  FooterContent,
+  Button,
+  PriceDetails,
+  Info,
+};
