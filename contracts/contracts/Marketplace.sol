2bb4fb61d37e9cc417c44ccc37f0ac2d85143a5a
matias-d <matiasc.02.2003@gmail.com>
2025-09-24 12:03:21 -0300

    docs: :memo: add comments to NFT and Marketplace contract

--- contracts/contracts/Marketplace.sol
+++ contracts/contracts/Marketplace.sol
@@ -5,23 +5,34 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
+
+/// @title NFT Marketplace
+/// @author matias-d
+/// @notice A decentralized marketplace to list, buy, and sell ERC721 NFTs with a fee.
+/// @dev Uses ReentrancyGuard to protect against reentrancy attacks.
+
 contract Marketplace is ReentrancyGuard {
 
-    address payable public immutable feeAccount;
-    uint public immutable feePercent;
-    uint public itemCount;
+    address payable public immutable feeAccount; // Address that receives marketplace fees
 
+    uint public immutable feePercent; // Fee percentage applied to each sale (e.g., 3 = 3%)
+
+    uint public itemCount; // Counter of items listed in the marketplace
+
+    /// @notice Represents an NFT listed in the marketplace
     struct Item {
-        uint itemId;
-        IERC721 nft;
-        uint tokenId;
-        uint price;
-        address payable seller;
-        bool sold;
+        uint itemId;               
+        IERC721 nft;               
+        uint tokenId;              
+        uint price;                
+        address payable seller;    
+        bool sold;                
     }
 
+    /// @notice Mapping that stores all listed items by their ID
     mapping(uint => Item) public items;
 
+    /// @notice Emitted when an NFT is listed for sale
     event Offered(
         uint itemId,
         address indexed nft,
@@ -29,6 +40,8 @@ contract Marketplace is ReentrancyGuard {
         uint price,
         address indexed seller
     );
+
+    /// @notice Emitted when an NFT is purchased
     event Bought(
         uint itemId,
         address indexed nft,
@@ -38,11 +51,17 @@ contract Marketplace is ReentrancyGuard {
         address indexed buyer
     );
 
+    /// @notice Initializes the marketplace with a fee percentage
+    /// @param _feePercent The percentage fee taken on each sale
     constructor (uint _feePercent) {
         feeAccount = payable(msg.sender);
         feePercent = _feePercent;
     }
 
+    /// @notice List an NFT for sale in the marketplace
+    /// @param _nft The address of the NFT contract
+    /// @param _tokenId The token ID of the NFT
+    /// @param _price The sale price in wei
     function makeItem(IERC721 _nft, uint _tokenId, uint _price) external nonReentrant {
         require(_price > 0, "Price must be greater than zero");
         itemCount++;
@@ -64,6 +83,8 @@ contract Marketplace is ReentrancyGuard {
         );
     }
 
+    /// @notice Retrieve information about a listed NFT
+    /// @param _itemId The marketplace ID of the item
     function getItem(uint _itemId)
     external
     view
@@ -77,9 +98,7 @@ contract Marketplace is ReentrancyGuard {
         string memory tokenURI
     )
     {
-
         Item storage item = items[_itemId];
-
         require(item.itemId != 0, "Item doesn't exist");
 
         return (
@@ -92,18 +111,23 @@ contract Marketplace is ReentrancyGuard {
             ERC721URIStorage(address(item.nft)).tokenURI(item.tokenId)
         );
     }
-    
 
+    /// @notice Purchase a listed NFT
+    /// @dev Transfers funds to the seller and feeAccount, then transfers the NFT to the buyer
+    /// @param _itemId The marketplace ID of the item to purchase
     function purchaseItem(uint _itemId) external payable nonReentrant {
         uint _totalPrice = getTotalPrice(_itemId);
         Item storage item = items[_itemId];
         require(_itemId > 0 && _itemId <= itemCount, "Item doesn't exist");
         require(msg.value >= _totalPrice, "Not enough ether");
         require(!item.sold, "Item already sold");
+
         item.seller.transfer(item.price);
         feeAccount.transfer(_totalPrice - item.price);
+
         item.sold = true;
         item.nft.transferFrom(address(this), msg.sender, item.tokenId);
+
         emit Bought(
             _itemId,
             address(item.nft),
@@ -114,8 +138,10 @@ contract Marketplace is ReentrancyGuard {
         );
     }
 
+    /// @notice Calculate the total price including marketplace fee
+    /// @param _itemId The marketplace ID of the item
     function getTotalPrice(uint _itemId) view public returns(uint) {
-        return ((items[_itemId].price*(100 + feePercent))/100);
+        return ((items[_itemId].price * (100 + feePercent)) / 100);
     }
 
 }
--- contracts/contracts/NFT.sol
+++ contracts/contracts/NFT.sol
@@ -3,15 +3,25 @@ pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
+/// @title Simple ERC721 NFT Contract
+/// @author matias-d
+/// @notice This contract allows users to mint unique NFTs with custom metadata URIs.
+/// @dev Inherits from OpenZeppelin's ERC721URIStorage for token storage and metadata handling.
 contract NFT is ERC721URIStorage {
 
-    uint public tokenCount;
-    constructor() ERC721("BotttsNeutralNFT", "BNFT"){}
+    uint public tokenCount; // Counter for the total number of tokens minted
+
+    /// @notice Initializes the NFT collection with a name and symbol
+    constructor() ERC721("PXLM", "PXL") {}
+
+    /// @notice Mint a new NFT with a given metadata URI
+    /// @dev Mints a new token to the caller and sets its URI
+    /// @param _tokenURI The metadata URI pointing to the NFT's data ( JSON file )
+    /// @return The ID of the newly minted token
     function mint(string memory _tokenURI) external returns (uint) {
         tokenCount++;
         _safeMint(msg.sender, tokenCount);
         _setTokenURI(tokenCount, _tokenURI);
         return tokenCount;
     }
-
 }
