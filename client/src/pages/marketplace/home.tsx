9e67e9a6ef13315a05722ad2bdd9e11fbb5a7ffa
matias-d <matiasc.02.2003@gmail.com>
2025-10-03 22:57:25 -0300

    .

--- client/src/pages/marketplace/home.tsx
+++ client/src/pages/marketplace/home.tsx
@@ -14,7 +14,7 @@ export default function Home() {
 
   return (
     <>
-      <PxlDetails />
+      {/* <PxlDetails /> */}
       <section className="pb-12">
         <LoadingTop loading={loading} />
 
